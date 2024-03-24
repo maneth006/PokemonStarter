58c2d69ec1b21c01769a27f9ed6a0467630932ed
maneth wijeratne <wijeratne.maneth@iisgalvanimi.edu.it>
2024-03-24 11:18:55 +0000

    commit 1

---
+++ pokemon/src/app/starter/starter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-starter',
+  standalone: true,
+  imports: [],
+  templateUrl: './starter.component.html',
+  styleUrl: './starter.component.css'
+})
+export class StarterComponent {
+
+}
