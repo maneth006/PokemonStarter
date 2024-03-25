04af5dfaa3ff217de882dd6dc4732f35f5527e76
maneth wijeratne <wijeratne.maneth@iisgalvanimi.edu.it>
2024-03-25 16:50:39 +0000

    commit non funziona il eventb

--- pokemon/src/app/starter/starter.component.ts
+++ pokemon/src/app/starter/starter.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-starter',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatIconModule],
   templateUrl: './starter.component.html',
   styleUrl: './starter.component.css'
 })
 export class StarterComponent {
+   
+  pagina = "home";
+  
+  onClickTurtwig(event: MouseEvent){
+    this.pagina = "TURTWIG";
+  }
 
+  
 }
